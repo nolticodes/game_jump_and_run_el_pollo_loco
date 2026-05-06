9d1b990f82f6c68025da32180da61e196cf24286
Denis Nolting <denis.nolting@gmx.de>
2026-05-06 15:16:39 +0200

    feat: added endscreen class

---
+++ classes/screens/endscreen.class.js
@@ -0,0 +1,74 @@
+class Endscreen {
+    restartButton;
+    mainMenuButton;
+
+    constructor(world) {
+        this.world = world;
+
+        this.menuWidth = 320;
+        this.menuHeight = 400;
+
+        this.menuX = (this.world.canvas.width - this.menuWidth) / 2;
+        this.menuY = (this.world.canvas.height - this.menuHeight) / 2;
+
+        this.buttonWidth = 200;
+        this.buttonHeight = 60;
+
+        this.buttonX = this.menuX + (this.menuWidth - this.buttonWidth) / 2;
+
+
+        this.restartButton = new Buttons(this.buttonX, this.menuY + 155, this.buttonWidth, this.buttonHeight, "Restart");
+        this.mainMenuButton = new Buttons(this.buttonX, this.menuY + 230, this.buttonWidth, this.buttonHeight, "Main Menu");
+    }
+
+    drawEndscreen() {
+        let ctx = this.world.ctx;
+        let canvas = this.world.canvas;
+        let endboss = this.world.level.enemies.find(enemy => enemy instanceof Endboss);
+
+        // Overlay
+        ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        // Shadow
+        this.drawRoundedRect(ctx, this.menuX, this.menuY + 8, this.menuWidth, this.menuHeight, 20, "#8B3A0E");
+        //Box
+        this.drawRoundedRect(
+            ctx,
+            this.menuX,
+            this.menuY,
+            this.menuWidth,
+            this.menuHeight,
+            20,              // radius
+            "#FFD54F"        // deine Wunschfarbe
+        );
+
+        // Schrift
+        if (this.world.character.isDead()) {
+            ctx.fillStyle = "#5A2A0A";
+            ctx.font = "42px 'Luckiest Guy', Arial";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText("You Lost", canvas.width / 2, 90);
+        } else if (endboss?.isDead()){
+            ctx.fillStyle = "#5A2A0A";
+            ctx.font = "42px 'Luckiest Guy', Arial";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText("You Won", canvas.width / 2, 90);
+        }
+
+
+
+        this.restartButton.draw(ctx);
+        this.mainMenuButton.draw(ctx)
+    }
+
+
+    screenWin() {
+
+    }
+
+    screenLost() {
+
+    }
+}
