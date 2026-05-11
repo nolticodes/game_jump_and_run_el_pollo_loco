ea42461bd6b1f3c497ec0966bd6e3c3602e21642
Denis Nolting <denis.nolting@gmx.de>
2026-05-11 15:18:43 +0200

    doc: jsdoc for level class

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,11 +1,40 @@
+/**
+ * Represents a game level containing all entities and environment elements.
+ */
 class Level {
+    /**
+     * List of enemy objects in the level.
+     */
     enemies;
-    clouds;
-    backgrounds;
-    coins;
-    bottles;
-    level_end_x = 4320
 
+    /**
+     * List of cloud objects for background animation.
+     */
+    clouds;
+
+    /**
+     * List of background layer objects.
+     */
+    backgrounds;
+
+    /**
+     * List of collectible coin objects.
+     */
+    coins;
+
+    /**
+     * List of collectible bottle objects.
+     */
+    bottles;
+
+    /**
+     * X-position marking the end of the level.
+     */
+    level_end_x = 4320;
+
+    /**
+     * Initializes the level with all required entities.
+     */
     constructor(enemies, clouds, backgrounds, coins, bottles) {
         this.enemies = enemies;
         this.clouds = clouds;
