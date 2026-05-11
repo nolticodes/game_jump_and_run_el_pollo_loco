18ccd14f673de2ac9706d5741feb3a1b3a1154ba
Denis Nolting <denis.nolting@gmx.de>
2026-05-11 15:21:47 +0200

    doc: jsdoc for coins class

--- classes/coins.class.js
+++ classes/coins.class.js
@@ -1,5 +1,11 @@
+/**
+ * Represents a collectible coin with floating animation.
+ */
 class Coins extends Moveableobject {
 
+    /**
+     * Collision offset for precise hit detection.
+     */
     offset = {
         top: 55,
         bottom: 55,
@@ -7,23 +13,24 @@ class Coins extends Moveableobject {
         right: 55,
     }
 
+    /**
+     * Initializes coin position, size and starts animation.
+     */
     constructor() {
         super();
         this.loadImage("./assets/img/8_coin/coin_1.png");
-
         this.x = 600 + Math.random() * 1500;
-
         this.baseY = 300 - Math.random() * 300; // Ausgangspunkt
         this.y = this.baseY;
-
         this.width = 150;
         this.height = 150;
-
         this.angle = 0; // für Sinus-Bewegung
-
         this.animate();
     }
 
+    /**
+     * Animates the coin with a vertical sinus movement.
+     */
     animate() {
         setInterval(() => {
             this.angle += 0.04; // Geschwindigkeit der Bewegung
