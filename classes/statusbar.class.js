303331e9b4e3d2be5eab140cb845d963eff3a56e
Denis Nolting <denis.nolting@gmx.de>
2026-05-11 15:27:19 +0200

    doc: js doc for statusbar

--- classes/statusbar.class.js
+++ classes/statusbar.class.js
@@ -1,3 +1,6 @@
+/**
+ * Represents a status bar for health, coins, bottles or endboss energy.
+ */
 class Statusbar extends DrawableObject {
 
     x = 50;
@@ -41,13 +44,14 @@ class Statusbar extends DrawableObject {
         "./assets/img/7_statusbars/2_statusbar_endboss/green/green100.png",
     ]
 
+    /**
+     * Initializes the status bar type, position and starting percentage.
+     */
     constructor(type, x, y, percentage) {
         super();
-
         this.y = y;
         this.x = x;
         this.percentage = percentage;
-
         if (type === "health") {
             this.images = this.imagesHealth;
         } else if (type === "coins") {
@@ -57,23 +61,28 @@ class Statusbar extends DrawableObject {
         } else if (type === "endboss") {
             this.images = this.imagesEndboss
         }
-
         this.loadImagesToCacheJSON(this.images);
         this.setPercentage(this.percentage);
     }
 
+    /**
+     * Updates the status bar percentage and displayed image.
+     */
     setPercentage(percentage) {
         this.percentage = percentage;
         let path = this.images[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
 
+    /**
+     * Resolves the correct image index based on the current percentage.
+     */
     resolveImageIndex() {
-        if (this.percentage > 80) return 5; // 100%
-        if (this.percentage > 60) return 4; // 80%
-        if (this.percentage > 40) return 3; // 60%
-        if (this.percentage > 20) return 2; // 40%
-        if (this.percentage > 0) return 1;  // 20%
-        return 0;                           // 0%
+        if (this.percentage > 80) return 5; 
+        if (this.percentage > 60) return 4;
+        if (this.percentage > 40) return 3;
+        if (this.percentage > 20) return 2;
+        if (this.percentage > 0) return 1; 
+        return 0;                         
     }
 }
