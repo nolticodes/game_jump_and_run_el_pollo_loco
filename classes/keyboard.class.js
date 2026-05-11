599375152201a07215e471b8adcd62e1aa077cf9
Denis Nolting <denis.nolting@gmx.de>
2026-05-11 15:16:42 +0200

    doc: jsdoc for keyboard class

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -1,9 +1,30 @@
+/**
+ * Represents the current state of user keyboard inputs.
+ */
 class Keyboard {
+    /**
+     * Indicates if the left movement key is pressed.
+     */
     left = false;
+    /**
+     * Indicates if the right movement key is pressed.
+     */
     right = false;
+    /**
+     * Indicates if the jump key is pressed.
+     */
     up = false; 
+    /**
+     * Indicates if the down key is pressed.
+     */
     down = false;
+    /**
+     * Indicates if the space key is pressed.
+     */ 
     space = false
+    /**
+     * Indicates if the throw action key is pressed.
+     */
     t = false
 }
 
