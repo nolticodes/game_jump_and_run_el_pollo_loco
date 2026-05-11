228933cc04eaab737f1cd12b681b3afb33f78ea2
Denis Nolting <denis.nolting@gmx.de>
2026-05-11 15:08:21 +0200

    doc: js for moveableObjects class

--- classes/moveableObject.class.js
+++ classes/moveableObject.class.js
@@ -1,3 +1,6 @@
+/**
+ * Base class for all moveable game objects with physics, animation and collision logic.
+ */
 class Moveableobject extends DrawableObject {
     speed = 0.2;
     otherDirection = false;
@@ -12,7 +15,11 @@ class Moveableobject extends DrawableObject {
     energy = 100;
     lastHit = 0;
     lastAnimation = "";
+    lastHit = 0;
 
+    /**
+     * Applies gravity to the object and updates vertical movement.
+     */
     applyGravity() {
         setInterval(() => {
             if (this.world && this.world.isPaused) return;
@@ -28,11 +35,16 @@ class Moveableobject extends DrawableObject {
         }, 1000 / 25)
     }
 
+    /**
+     * Checks if the object is currently above ground level.
+     */
     isInAir() {
         return this.y < 40
     }
 
-
+    /**
+     * Moves the object continuously to the left.
+     */
     animationMoveLeft() {
         setInterval(() => {
             if (this.world && this.world.isPaused) return;
@@ -40,19 +52,23 @@ class Moveableobject extends DrawableObject {
         }, 1000 / 60)
     }
 
+    /**
+     * Plays a looping animation sequence.
+     */
     playAnimation(images, animationName) {
-
         if (this.lastAnimation !== animationName) {
             this.currentImage = 0;
             this.lastAnimation = animationName;
         }
-
         let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
 
+    /**
+     * Plays an animation once and stops on the last frame.
+     */
     playAnimationOnce(images, animationName) {
         if (this.lastAnimation !== animationName) {
             this.currentImage = 0;
@@ -69,19 +85,30 @@ class Moveableobject extends DrawableObject {
         }
     }
 
-
+    /**
+     * Moves the object to the right.
+     */
     moveRight() {
         this.x += this.speed;
     }
 
+    /**
+     * Moves the object to the left.
+     */
     moveLeft() {
         this.x -= this.speed;
     };
 
+    /**
+     * Triggers a jump by setting vertical speed.
+     */
     jump() {
         return this.speedY = 25
     };
 
+    /**
+     * Checks collision with another moveable object.
+     */
     isColliding(mo) {
         return this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
             this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
@@ -89,8 +116,9 @@ class Moveableobject extends DrawableObject {
             this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom
     }
 
-    lastHit = 0;
-
+    /**
+     * Applies damage from another object and updates state.
+     */
     hit(mo) {
         this.energy -= mo.damage || 0;
 
@@ -106,12 +134,18 @@ class Moveableobject extends DrawableObject {
         this.lastKeyPressTime = now;
     }
 
+    /**
+     * Checks if the object is currently in a hurt state.
+     */
     isHurt() {
         let timepassed = new Date().getTime() - this.lastHit;
         timepassed = timepassed / 1000;
         return timepassed < 1;
     }
 
+    /**
+     * Checks if the object has no remaining energy.
+     */
     isDead() {
         return this.energy == 0
     }
