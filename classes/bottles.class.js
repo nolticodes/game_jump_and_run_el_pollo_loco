aed80604221dbe5c87befab9b0b9c9fa861e8310
Denis Nolting <denis.nolting@gmx.de>
2026-05-11 15:20:39 +0200

    doc: jsdoc for bottles class

--- classes/bottles.class.js
+++ classes/bottles.class.js
@@ -1,7 +1,14 @@
+/**
+ * Represents a collectible bottle that can be picked up and used as a throwable object.
+ */
 class Bottles extends Moveableobject {
-    
+    /**
+     * Damage dealt when the bottle hits an enemy.
+     */
     damage = 20;
-
+    /**
+     * Collision offset for more precise hit detection.
+     */
     offset = {
         top: 20,
         bottom: 0,
@@ -9,6 +16,9 @@ class Bottles extends Moveableobject {
         right: 20,
     }
 
+    /**
+     * Animation frames for bottle rotation while being thrown.
+     */ 
     bottleThrow = [
         "./assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
         "./assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
@@ -16,6 +26,9 @@ class Bottles extends Moveableobject {
         "./assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
     ]
 
+    /**
+     * Animation frames for bottle splash after impact.
+     */
     bottleSplash = [
         "./assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
         "./assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
@@ -25,22 +38,25 @@ class Bottles extends Moveableobject {
         "./assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
     ]
 
+    /**
+     * Initializes bottle position, size and loads animation assets.
+     */
     constructor() {
         super().loadImage("./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
         this.loadImagesToCacheJSON(this.bottleThrow);
         this.loadImagesToCacheJSON(this.bottleSplash);
-
         this.x = 600 + Math.random() * 1000;
         this.y = 330 + Math.random() * 10;
         this.width = 80;
         this.height = 100;
     }
 
+    /**
+     * Plays the splash animation and marks the bottle for deletion afterwards.
+     */
     playSplashAnimation() {
         this.isBroken = true;
-
         let i = 0;
-
         let interval = setInterval(() => {
             if (i < this.bottleSplash.length) {
                 let path = this.bottleSplash[i];
